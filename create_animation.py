66bc4fc2b7440848a78191fb7510649c4653a386
jannis <jannisko99@gmail.com>
2020-04-02 16:23:51 +0000

    add example animation function

---
+++ create_animation.py
@@ -0,0 +1,75 @@
+from differential_evolution import DifferentialEvolution
+from differential_evolution.losses import create_huber
+from differential_evolution.models import create_quadratic
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation, cm
+import sys
+
+def animate_models(
+    output_gif=True,
+    output_mp4=False,
+    frame_num=300,
+    fps=30
+):
+
+    x = [1,2,3,4,5,6,7,8]
+    y = [1,4,9,16,25,36,49,64]
+
+    a = tf.Variable(0.0)
+    b = tf.Variable(0.0)
+    c = tf.Variable(0.0)
+
+    model = create_quadratic(x, a, b, c)
+
+    loss = create_huber(model, y)
+
+    fig = plt.figure()
+    ax = fig.gca()
+
+    plot_model = create_quadratic(np.linspace(0,10,100), a, b, c)
+
+    opt = DifferentialEvolution(loss, [a,b,c])
+
+    def animate(i):
+        opt.next_generation()
+        plt.cla()
+        for point in opt.current_population:
+            a.assign(point[0][0])
+            b.assign(point[1][0])
+            c.assign(point[2][0])
+            ax.plot(np.linspace(0,10,100), plot_model(), zorder=-1)
+        ax.scatter(x,y, zorder=1, marker='^', c='000000')
+        sys.stdout.write("\r%d%%" % ((i/frame_num)*100))
+        sys.stdout.flush()
+        ax.set_ylim((0, 70))
+        ax.set_xlim((0, 10))
+        plt.xlabel('x')
+        plt.ylabel('x²')
+        print(fig.get_size_inches()*fig.dpi)
+        plt.text(5, 72, 
+            f'Frame: {i:03}/{frame_num}; Best loss: {opt.get_best_point()[1]:08.5f}'
+        )
+        #plt.title('An example of a quadratic regression using differential evolution')
+        return fig,
+        
+
+    def init():
+        return fig,
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                frames=frame_num, interval=100, blit=True)
+
+
+    if(output_gif):
+        anim.save('quadratic_models.gif', fps=fps, writer='imagemagick')
+
+    if(output_mp4):
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=fps, bitrate=5000)
+        anim.save('surface_rotation.mp4', writer=writer)
+
+
+if __name__ == "__main__":
+    animate_models()
